c7b7fb94e9f0de6bbfaa7b3a5baba9b10d5b45b4
Darknees01 <Narutosenin101@gmail.com>
2016-09-09 12:18:57 -0500

    Add files via upload

---
+++ al revez.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+int main()
+{
+    string cadena = 0;
+    string aux_cadena = "";
+ 
+    for (int i = cadena.size(); i >=0 ; i--)
+         aux_cadena += cadena[i];
+ 
+    cout<<"El texto normal es: "<<cadena<<endl;
+    cout<<"El texto invertido es: "<<aux_cadena<<endl;
+}
